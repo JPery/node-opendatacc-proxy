65e9c801dcc3a0e73ee69ab85f422cd07a2b8608
fdiazgon <fediazgon@gmail.com>
2016-06-26 20:57:27 +0200

    Adding files

---
+++ README.md
@@ -0,0 +1,67 @@
+## Nodejs Opendata Cáceres proxy
+
+A nodejs server that acts like a proxy to the Opendata Cáceres portal.
+
+Currently, you can query the following datasets: [cinemas](opendata.caceres.es/dataset/cines-caceres), [monuments](http://opendata.caceres.es/dataset/monumentos-caceres), [museums](http://opendata.caceres.es/dataset/museos-caceres), [parking spaces for disabled](http://opendata.caceres.es/dataset/plazas-de-movilidad-reducida-caceres), [restaurants](http://opendata.caceres.es/dataset/restaurantes-caceres) and [theaters](opendata.caceres.es/dataset/teatros-caceres). 
+
+The queries used are in the opendatacc-endpoint.js file, you can modify these based on the information you want to obtain from the datasets. The results are cached, so after the first query to one particular dataset, the subsequent queries should return the information instantly.
+
+## Demo
+
+![](https://lh3.googleusercontent.com/T7IAUdQGIh3UkETz9Ng4A3stH6H308kw-lBGxXl43k12jSm4RJi3WYAKWRU2rNYMOqf9Ov42l-MRH8_7-8TcTUYrKdmwMkqGbDMYoxMhyqchq8MZoIj-ndc2kOmeZ7DbJJHflumxOwxbC8JFor4B3tfMYx2rFRTIcgZ8UbYvocceQn8nA_cIQ_SkpIDvugUOiXUA4SxGhnXuJD0XZBH2-81jC7W5CJ5CG0Rt1sLktqRAp-3mg_vttBEe059O9Pjcgqw9zmGPZYGRo2BZWcVT9lLSF05sIdbudb-0O-jmKJZCZdanndeTKII5XmyL6B1az1v2ULwue5vfwcH64TpdkhSYykr9duix9vN13-cHfscmtvSxMEW6z8aOb-rVTcDieoynCZuLCFsKP3e1L7Z_qEkTXem9yz6RmAPN36aFPAxIZFg4w2xKyuHqtG_785jpIrZX3p3L60IJr-UUk5ovvlpB6xqavnhxcAMeAcPh1oTNtgh3d5v4_2-9hCoFUL3LGPGv7-S2NFb-Jcn_gR50maiNTT-BQI1OnD8IOVazSjV4w-fJEeMHvuHXpDzTysSJ1LBULgyWkiVOlsd0q_c3RqZFKYZV9jE=w297-h480-no)
+
+
+## Run the server locally
+
+Clone this repository or download and extract the project to where you want to work.
+
+### Install dependencies:
+
+1)  Check your Node.js version.
+
+```sh
+node --version
+```
+
+2)  If you don't have Node.js installed go to [nodejs.org](https://nodejs.org) and click on the big green Install button.
+
+3)  Install `npm` dependencies.
+
+```sh
+cd node-opendatacc-proxy && npm install
+```
+
+## Test the server
+
+```sh
+node app.js
+```
+
+In a web browser, type in the address bar localhost:3000/<dataset_name>, for example, if you want the information of al the museum in the city of Cáceres go to: localhost:3000/theater. This should be the response:
+
+```json
+[
+   {
+      "nombre":{
+         "type":"literal",
+         "xml:lang":"es",
+         "value":"Gran Teatro"
+      },
+      "lat":{
+         "type":"typed-literal",
+         "datatype":"http://www.w3.org/2001/XMLSchema#decimal",
+         "value":"39.473166"
+      },
+      "long":{
+         "type":"typed-literal",
+         "datatype":"http://www.w3.org/2001/XMLSchema#decimal",
+         "value":"-6.375632"
+      },
+      "tieneEnlaceSIG":{
+         "type":"uri",
+         "value":"http://sig2.caceres.es/SerWeb/fichatoponimo.asp?mslink=2398"
+      },
+      "image":"http://sig2.caceres.es/fotosOriginales/toponimia/GRAN_TEATRO_01.jpg"
+   }
+]
+```
---
+++ opendatacc-endpoint.js
@@ -0,0 +1,174 @@
+// Teatros
+var teatrosQuery = 'select ?nombre ?lat ?long ?tieneEnlaceSIG where {?uri a om:Teatro. ?uri rdfs:label ?nombre.?uri geo:lat ?lat.?uri geo:long ?long OPTIONAL{?uri om:tieneEnlaceSIG ?tieneEnlaceSIG.}}'
+
+// Cines
+var cinesQuery = 'select ?nombre ?lat ?long ?tieneEnlaceSIG where{?uri a om:Cine. ?uri rdfs:label ?nombre.?uri geo:lat ?lat.?uri geo:long ?long OPTIONAL{?uri om:tieneEnlaceSIG ?tieneEnlaceSIG.}}'
+
+// Monumentos
+var monumentosQuery = 'select ?nombre ?lat ?long ?tieneEnlaceSIG ?enlacedbpedia where { ?uri a om:Monumento. ?uri rdfs:label ?nombre.?uri geo:lat ?lat.?uri geo:long ?long OPTIONAL{?uri om:tieneEnlaceSIG ?tieneEnlaceSIG} OPTIONAL{?uri owl:sameAs ?enlacedbpedia.}}'
+
+// Museos
+var museosQuery = 'select ?nombre ?lat ?long ?tieneEnlaceSIG ?descripcion ?enlacedbpedia where {?uri a om:Museo. ?uri rdfs:label ?nombre.?uri geo:lat ?lat.?uri geo:long ?long OPTIONAL{?uri om:tieneEnlaceSIG ?tieneEnlaceSIG} OPTIONAL{?uri owl:sameAs ?enlacedbpedia} OPTIONAL{?uri schema:description ?descripcion.}}'
+
+// PlazaMovilidadReducida
+var movilidadQuery = 'select ?uri ?lat ?long ?tieneEnlaceSIG ?descripcion where {?uri a om:PlazaMovilidadReducida. ?uri rdfs:label ?nombre.?uri geo:lat ?lat.?uri geo:long ?long OPTIONAL{?uri om:tieneEnlaceSIG ?tieneEnlaceSIG} OPTIONAL{?uri rdfs:comment ?descripcion.}}'
+
+// Bares restaurantes y cafes
+var cafeRestauranteBarQuery = 'select ?nombre ?lat ?long where {{?uri a om:BarCopas. ?uri rdfs:label ?nombre.?uri geo:lat ?lat.?uri geo:long ?long.} union {?uri a om:CafeBar. ?uri rdfs:label ?nombre.?uri geo:lat ?lat.?uri geo:long ?long.} union{?uri a om:Restaurante. ?uri rdfs:label ?nombre.?uri geo:lat ?lat.?uri geo:long ?long.}}'
+
+/*
+ * We have to:
+ 	1. Get all the information of the selected dataset with a query to the SPARQL endpoint of opendata Cáceres
+ 	2. For every element of the dataset, get an image from it's 'enlaceSIG'
+ 		2.1. We add the image as a property of the place (place.image). 
+ 			If no image was found, this property is undefined.
+   Both calls are asynchronous, and first call must finish before second call is performed
+ */
+
+var async = require('async'), // Do calls in series
+    request = require('request'), // Do http request (async)
+    cheerio = require('cheerio'); // Get img tags from external url
+NodeCache = require("node-cache"); // Save query results on the cache for one day.
+
+var myCache = new NodeCache({ stdTTL: 86400, checkperiod: 3600 });
+
+module.exports = {
+
+    /**
+     * Get all the elements of one particular dataset of Cáceres
+     * @param {string} whichDataset - One of { monument, museum, restaurant, parking, theater, cinema }
+     */
+    getDataCaceres: function(whichDataset, allDoneCallback) {
+
+        async.waterfall(
+            [
+                // 1. Get all the information of the selected dataset with a query to the SPARQL endpoint of opendata Cáceres
+                function(callback) {
+                    var data;
+                    try {
+                        data = myCache.get(whichDataset, true);
+                        // Finish, since image urls are also included in the cache
+                        allDoneCallback(data);
+                    } catch (err) {
+                        // Not in the cache.
+                        getDataFromEndpoint(whichDataset, function dataObtainedCallback(bindings) {
+                            callback(null, bindings); // get the images
+                        });
+                    }
+                },
+
+                // 2. For every element of the dataset, get an image from it's 'enlaceSIG'
+                function(bindings, callback) {
+                    addImageToBindings(bindings, callback);
+                },
+            ],
+
+            // Final callback function
+            function(err, results) {
+                // If this point is reached, results are not in the cache. Store them
+                if (results.length)
+                    myCache.set(whichDataset, results);
+                allDoneCallback(results);
+            }
+        );
+
+    }
+};
+
+
+function getDataFromEndpoint(whichDataset, dataObtainedCallback) {
+
+    var endpoint = 'http://opendata.caceres.es/sparql/';
+    var graph = '';
+    var SPARQLquery = '';
+    switch (whichDataset) {
+        case 'monument':
+            SPARQLquery = monumentosQuery;
+            break;
+        case 'museum':
+            SPARQLquery = museosQuery;
+            break;
+        case 'restaurant':
+            SPARQLquery = cafeRestauranteBarQuery;
+            break;
+        case 'parking':
+            SPARQLquery = movilidadQuery;
+            break;
+        case 'theater':
+            SPARQLquery = teatrosQuery;
+            break;
+        case 'cinema':
+            SPARQLquery = cinesQuery;
+            break;
+        default:
+            break;
+    }
+
+    if (SPARQLquery) {
+
+        request({
+                url: 'http://opendata.caceres.es/sparql/',
+                qs: { // Query string data
+                    'default-graph-uri': graph,
+                    query: SPARQLquery,
+                    format: 'json'
+                },
+            },
+            function(error, response, body) {
+                if (error) {
+                    console.log(error);
+                } else {
+                    var jsonBody = JSON.parse(body);
+                    dataObtainedCallback(jsonBody.results.bindings);
+                }
+            });
+
+    } else { // return empty object if bad dataset was selected. 
+        dataObtainedCallback([]);
+    }
+}
+
+function addImageToBindings(bindings, allImagesObtainedCallback) {
+
+    var calls = [];
+
+    bindings.forEach(function(place) {
+
+        var enlaceSIG = place.tieneEnlaceSIG;
+
+        if (enlaceSIG)
+            calls.push(function(callback) {
+                fetchImageFromSIGLink(enlaceSIG.value, function(placeImage) {
+                    if (placeImage)
+                        place.image = placeImage;
+                    callback(null, place);
+                })
+            });
+    });
+
+    async.parallel(calls, function(err, result) {
+        // This code will run after all calls finished the job or when any of the calls passes an error
+        allImagesObtainedCallback(null, bindings);
+    });
+}
+
+function fetchImageFromSIGLink(sigLink, callback) {
+
+    var pattern = new RegExp('\/fotosOriginales\/toponimia\/');
+
+    request(sigLink, function(err, resp, body) {
+
+        $ = cheerio.load(body);
+        images = $('img'); // jquery get all imgs
+
+        var placeImage;
+        for (var i = 0; i < images.length; i++) {
+            var imageSrc = $(images[i]).attr('src');
+            if (pattern.test(imageSrc)) {
+                placeImage = 'http://sig2.caceres.es' + imageSrc;
+                break;
+            }
+        }
+        callback(placeImage);
+    });
+}
